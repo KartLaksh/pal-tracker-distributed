c5a840767915af8b159cf40adbc821204a8214db
Karthik <Karthikeyan.Veerabathiran@Libertymutual.com>
2020-05-01 15:49:04 -0400

    Completed - Securing a Distributed System

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -12,10 +12,10 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestConfig {
     @LoadBalanced
-    @Bean
-    public RestOperations restOperations() {
-        return new RestTemplate();
-    }
+//    @Bean
+//    public RestOperations restOperations() {
+//        return new RestTemplate();
+//    }
 
     @Bean
     public ObjectMapper objectMapper() {
--- integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
@@ -36,6 +36,7 @@ public class ApplicationServer {
         String dbUrl = "jdbc:mysql://localhost:3306/" + dbName + "?useSSL=false&useTimezone=true&serverTimezone=UTC&useLegacyDatetimeCode=false";
 
         start(envMapBuilder()
+            .put("APPLICATION_OAUTH_ENABLED", "false")
             .put("SPRING_DATASOURCE_URL", dbUrl)
             //.put("REGISTRATION_SERVER_ENDPOINT", "http://localhost:8883")
             .put("EUREKA_CLIENT_ENABLED", "false")
